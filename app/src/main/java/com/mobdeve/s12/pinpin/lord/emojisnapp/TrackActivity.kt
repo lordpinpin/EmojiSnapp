7341147c72bef1dd831988b1ec2996ff13822ae7
Harvey Chua <harvs89.2@gmail.com>
2024-12-09 16:51:44 +0800

    feat: implement level tracking

--- app/src/main/java/com/mobdeve/s12/pinpin/lord/emojisnapp/TrackActivity.kt
+++ app/src/main/java/com/mobdeve/s12/pinpin/lord/emojisnapp/TrackActivity.kt
@@ -15,13 +15,17 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import com.mobdeve.s12.pinpin.lord.emojisnapp.databinding.ActivityTrackBinding
 
 class TrackActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTrackBinding
     private var trackAdapter: TrackAdapter? = null
 
-
+    private val instance = FirebaseDatabase.getInstance("https://mco3-7e1e6-default-rtdb.asia-southeast1.firebasedatabase.app/")
+    private val database = instance.getReference("matches")
+    private val user = FirebaseAuth.getInstance().currentUser;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -35,8 +39,31 @@ class TrackActivity : AppCompatActivity() {
         val layoutManager = LinearLayoutManager(this)
         layoutManager.reverseLayout = true
         layoutManager.stackFromEnd = true
-        val progress = intent.getIntExtra("CUR_LEVEL", 25)
+        var progress = 0
+        write(emojiList, layoutManager, progress)
 
+
+        database.get().addOnSuccessListener {
+            val idk = it.value as Map<String, Map<String, Any>>
+
+            var cur_pts = 0L
+            idk.forEach {
+                val playerName = it.value["player1"] as String
+                val points = it.value["player1Points"] as Long
+
+                if(playerName == user?.uid) {
+                    cur_pts += points
+                    progress = Math.max(progress, cur_pts.toInt())
+                }
+            }
+
+            Log.e("Progress", "Change to " + progress)
+            write(emojiList, layoutManager, progress)
+        }
+
+    }
+
+    fun write(emojiList: List<Emoji>, layoutManager: RecyclerView.LayoutManager, progress: Int) {
         trackAdapter = TrackAdapter(emojiList, progress, this)
 
         binding.collectionRv.layoutManager = layoutManager
@@ -53,5 +80,4 @@ class TrackActivity : AppCompatActivity() {
             finish() // Ends the current activity
         }
     }
-
 }
